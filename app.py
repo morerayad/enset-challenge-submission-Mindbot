bdf3f57e3935187ebf41a7985cdb2ea605dc7143
azeddine <azeddinejimi@gmail.com>
2026-04-05 23:25:29 +0100

    improve main logic and Streamlit app

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
 import streamlit as st
 from main import run_agents  # ou run_agents.py si tu changes le nom
+# Configure the page then Display the main title of the app 
+st.set_page_config(page_title="AI Learning Assistant", page_icon="🤖") 
 
-st.set_page_config(page_title="AI Learning Assistant", page_icon="🤖")
+st.title("🤖 AI Learning Assistant" )
+st.write("Analyze your problem and get a smart solution using AI Agents" )
 
-st.title("🤖 AI Learning Assistant")
-st.write("Analyze your problem and get a smart solution using AI Agents")
+user_input = st.text_area("✍️ Describe your problem: ")
 
-user_input = st.text_area("✍️ Describe your problem:")
+if st.button("🚀 Analyze") :
+    if user_input :
+        with st.spinner("Analyzing...") :
+            result =run_agents(user_input)
 
-if st.button("🚀 Analyze"):
-    if user_input:
-        with st.spinner("Analyzing..."):
-            result = run_agents(user_input)
 
-        st.success("Analysis complete ✅")
-        st.subheader("📊 Analysis")
-        st.info(result["analysis"])
+        st.success(" Analysis complete ✅")
+        st.subheader(" 📊 Analysis")
+        st.info(result[" analysis"])
 
-        st.subheader("💡 Suggested Solution")
-        st.success(result["solution"])
-    else:
-        st.warning("⚠️ Please enter a problem first!")
+        st.subheader(" 💡 Suggested Solution")
+        st.success(result[" solution"])
+    else :
+        st.warning(" ⚠️ Please enter a problem first!")
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
+
+# Function takes user input and returns analysis and solution
 def run_agents(user_input):
-    # Mock analysis
-    analysis = f"Analysis of your problem: '{user_input}' shows main difficulties and points to focus on."
+    # Create a simple analysis message
+    analysis = f"Analysis of your problem: '{user_input}' shows main difficulties and points to focus on . "
     
-    # Mock solution
-    solution = f"Suggested solution: try breaking the problem into smaller steps and practice regularly."
+    # Create a simple suggested solution for the user
+    solution = f"Suggested solution: try breaking the problem into smaller steps and practice regularly . "
     
     return {
         "analysis": analysis,
         "solution": solution
     }
-
+# it;s runs only when file is executed directly 
 if __name__ == "__main__":
-    result = run_agents("I can't understand math")
+    result = run_agents(" I can't understand math ")
     print(result)
